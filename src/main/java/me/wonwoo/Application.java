4576d48b5f177f28db26bc0f57311d8a54f35cdf
aoruqjfu@naver.com <dheh123>
2015-12-07 21:03:57 +0900

    banner 추가

--- src/main/java/me/wonwoo/Application.java
+++ src/main/java/me/wonwoo/Application.java
@@ -1,46 +1,42 @@
 package me.wonwoo;
 
-import java.util.concurrent.TimeUnit;
-
-import me.wonwoo.hello.QAccounts;
-
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.ErrorPage;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class Application {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
-	/**
-	 * 톰캣 포트 변경
-	 * 
-	 * @return
-	 */
-	@Bean
-	public EmbeddedServletContainerFactory servletContainer() {
-		TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
-		factory.setPort(8090);
-		factory.setSessionTimeout(10, TimeUnit.MINUTES);
-		// factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND,
-		// "/notfound.html"));
-		return factory;
-	}
+    /**
+     * 톰캣 포트 변경
+     *
+     * @return
+     */
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+        factory.setPort(8090);
+        factory.setSessionTimeout(10, TimeUnit.MINUTES);
+        // factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND,
+        // "/notfound.html"));
+        return factory;
+    }
 
-	@Bean
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
-	}
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 
-	public static void main(String[] args) throws InterruptedException {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(Application.class, args);
+    }
 }
