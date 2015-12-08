f653044a0cd01a805f277471fe8525342a17af7f
wonwoo <aoruqjfu@naver.com>
2015-12-08 17:53:20 +0900

    Test code

--- src/main/java/me/wonwoo/account/AccountsController.java
+++ src/main/java/me/wonwoo/account/AccountsController.java
@@ -2,6 +2,7 @@ package me.wonwoo.account;
 
 import lombok.extern.slf4j.Slf4j;
 import me.wonwoo.config.Config;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -9,13 +10,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
+import static org.junit.Assert.*;
 
 @RestController
 @Slf4j
@@ -33,6 +35,7 @@ public class AccountsController {
 	@RequestMapping(value = "/accounts/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> getAccount(@PathVariable Long id) {
 		Accounts saveAccounts = service.getAccount(id);
+		assertNotNull(" accounts is null", saveAccounts);
 		return new ResponseEntity<>(saveAccounts, HttpStatus.OK);
 	}
 
