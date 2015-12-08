677a6c3bc86609d9e92781a509cec0217d0e5282
wonwoo <aoruqjfu@naver.com>
2015-12-08 10:06:55 +0900

    트랜젝션 추가

--- src/main/java/me/wonwoo/hello/AccountsService.java
+++ src/main/java/me/wonwoo/hello/AccountsService.java
@@ -2,13 +2,12 @@ package me.wonwoo.hello;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class AccountsService {
 
 	@Autowired
