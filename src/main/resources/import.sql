7748a2391857855f29a729b166cd4b1a91113b7c
aoruqjfu@naver.com <dheh123>
2015-12-23 23:23:02 +0900

    react 상세 추가

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,4 @@
 insert into accounts(email, first_name, last_name,status, password) values ('aoruqjfu@gmail.com','wonwoo', 'lee','001','$2a$10$nfC6f2Ka855DYshbOuEJbeZtpx4lSLwhZEVh6XcySRji/3IbZTr6O');
 insert into category(name) values('첫번째 카테고리');
 insert into post(title, content, email, category_id) values('첫번째포스트','첫번째포스트 내용', 'aoruqjfu@gmail.com',1);
+insert into post(title, content, email, category_id) values('두번째포스트','두번째포스트 내용', 'aoruqjfu@gmail.com',1);
--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,4 +1,4 @@
-const {Router, Route, Link} = ReactRouter;
+const {Router, Route, Link, IndexRoute} = ReactRouter;
 
 var HeadNav = React.createClass({
 	render : function(){
@@ -33,68 +33,6 @@ var HeadNav = React.createClass({
 	}
 });
 
-var Content = React.createClass({
-	getInitialState: function() {
-		return {
-			posts : [],
-		};
-	},
-	componentDidMount: function() {
-		$.get(this.props.url, function(result) {
-			var content = result.content;
-			this.setState({
-				posts: content
-			});
-		}.bind(this));
-	},
-	render : function(){
-		return (
-			<ContentPost data={this.state.posts} />
-		);
-	}
-});
-
-var ContentPost = React.createClass({
-	render :function(){
-		var PostNode = this.props.data.map(function (post) {
-			return (
-				<div>
-					<h2>
-						<Link to="/about">{post.title}</Link>
-					</h2>
-					<p className="lead">
-						by <a href="index.php">{post.email}</a>
-					</p>
-					<p><span className="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
-					<hr>
-	                    <img className="img-responsive" src="http://placehold.it/900x300" alt=""/>
-	                </hr>
-	                    <p>{post.content}</p>
-                        <a className="btn btn-primary" href="#">Read More <span className="glyphicon glyphicon-chevron-right"></span></a>
-	                
-                </div>
-            )
-		});
-		return (
-			<div className="col-md-8">
-				<h1 className="page-header">
-					Page Heading
-				</h1>
-				{PostNode}
-				<hr>
-					<ul className="pager">
-		                <li className="previous">
-		                	<a href="#">&larr; Older</a>
-		                </li>
-		                <li className="next">
-		                	<a href="#">Newer &rarr;</a>
-		                </li>
-		            </ul>
-	            </hr>
-			</div>
-		);
-	}
-});
 
 
 var Search = React.createClass({
@@ -181,52 +119,188 @@ var Footer = React.createClass({
 const Main = React.createClass({
 	render : function(){
 		return (
-			<div>
-				<HeadNav url=""/>
-                <div className="container">
-                    <div className="row">
-                        <Content url="/post"/>
-                        <LeftNav />
-                    </div>
-                    <Footer />
-                </div>
-                {this.props.children}
+             <Contents url="/post"/>
+		)
+	}
+});
+
+
+var Contents = React.createClass({
+	getInitialState: function() {
+		return {
+			posts : [],
+		};
+	},
+	componentDidMount: function() {
+		$.get(this.props.url, function(result) {
+			var content = result.content;
+			this.setState({
+				posts: content
+			});
+		}.bind(this));
+	},
+	render : function(){
+		return (
+			<ContentsPost data={this.state.posts} />
+		);
+	}
+});
+
+var ContentsPost = React.createClass({
+	render :function(){
+		var PostNode = this.props.data.map(function (post) {
+			return (
+				<div>
+					<h2>
+						<Link to={`/content/${post.id}`}> {post.title}</Link>
+					</h2>
+					<p className="lead">
+						by <a href="index.php">{post.email}</a>
+					</p>
+					<p><span className="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
+					<hr>
+						<img className="img-responsive" src="http://placehold.it/900x300" alt=""/>
+					</hr>
+					<p>{post.content}</p>
+					<a className="btn btn-primary" href="#">Read More <span className="glyphicon glyphicon-chevron-right"></span></a>
+					<hr></hr>
+				</div>
+			)
+		});
+		return (
+			<div className="col-md-8">
+				<h1 className="page-header">
+					Page Heading
+				</h1>
+				{PostNode}
+				<ul className="pager">
+					<li className="previous">
+						<a href="#">&larr; Older</a>
+					</li>
+					<li className="next">
+						<a href="#">Newer &rarr;</a>
+					</li>
+				</ul>
+			</div>
+		);
+	}
+});
+
+
+var Content = React.createClass({
+	getInitialState: function() {
+		return {
+			post : {},
+		};
+	},
+	componentDidMount: function() {
+		$.get("post/" + this.props.params.id, function(result) {
+			var content = result;
+			this.setState({
+				post: content
+			});
+		}.bind(this));
+	},
+	render : function(){
+		return (
+			<ContentPost data={this.state.post} />
+		);
+	}
+});
+
+var ContentPost = React.createClass({
+	render : function(){
+		return (
+			<div className="col-md-8">
+				<h1>{this.props.data.title}</h1>
+				<p className="lead">
+					by <a href="#">{this.props.data.email}</a>
+				</p>
+				<hr>
+				</hr>
+				<p><span className="glyphicon glyphicon-time"></span> Posted on August 24, 2013 at 9:00 PM</p>
+
+				<hr>
+				</hr>
+
+				<img className="img-responsive" src="http://placehold.it/900x300" alt=""/>
+				<hr>
+					<p className="lead">{this.props.data.content}</p>
+				</hr>
+				<Comments />
 			</div>
 		)
 	}
 });
 
+var Comments = React.createClass({
+	render : function(){
+		return(
+			<div>
+				<CommentForm />
+				<CommentBox />
+			</div>
+		)
+	}
+});
+
+var CommentForm = React.createClass({
+	render : function(){
+		return (
+			<div className="well">
+				<h4>Leave a Comment:</h4>
+				<form role="form">
+					<div className="form-group">
+						<textarea className="form-control" rows="3"></textarea>
+					</div>
+					<button type="submit" className="btn btn-primary">Submit</button>
+				</form>
+			</div>
+		)
+	}
+});
+
+var CommentBox = React.createClass({
+	render : function(){
+		return (
+			<div className="media">
+				<a className="pull-left" href="#">
+					<img className="media-object" src="http://placehold.it/64x64" alt="" />
+					</a>
+					<div className="media-body">
+						<h4 className="media-heading">Start Bootstrap
+							<small>August 25, 2014 at 9:30 PM</small>
+						</h4>
+						Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+					</div>
+				</div>
+		)
+	}
+});
+
+
 const App = React.createClass({
 	render : function(){
 		return (
-			<div>123</div>
+			<div>
+				<HeadNav url=""/>
+				<div className="container">
+					<div className="row">
+						{this.props.children}
+						<LeftNav />
+					</div>
+					<Footer />
+				</div>
+			</div>
 		)
 	}
 });
-const About = React.createClass({
-	componentDidMount: function() {
-		console.log(this.props.router);
-	},
-	render : function(){
-		return (
-			<div>1255555555555553</div>
-		)
-	}
-});
-const Users = React.createClass({
-	render : function(){
-		return (
-			<div>12zzzzzzzzzzzzzz3</div>
-		)
-	}
-})
 
 ReactDOM.render((
   <Router>
-    <Route path="/" component={Main}>
-      <Route path="about" component={About} />
-      <Route path="inbox" component={Users}>
-      </Route>
+    <Route path="/" component={App}>
+	  <IndexRoute component={Main}/>
+      <Route path="content/:id" component={Content} />
     </Route>
   </Router>
 ), document.body)
