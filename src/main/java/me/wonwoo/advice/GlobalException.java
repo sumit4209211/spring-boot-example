3850117a0c53c6c1a5aefc89b24476b0b7eba992
aoruqjfu@naver.com <dheh123>
2016-01-11 01:21:30 +0900

    api error

--- src/main/java/me/wonwoo/advice/GlobalException.java
+++ src/main/java/me/wonwoo/advice/GlobalException.java
@@ -1,52 +1,52 @@
-package me.wonwoo.advice;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
-import me.wonwoo.exception.DataNotFoundException;
-import me.wonwoo.exception.BadRequestException;
-import me.wonwoo.exception.DuplicateException;
-import me.wonwoo.exception.bean.AdviceErrorRespones;
-
-@ControllerAdvice
-@RestController
-public class GlobalException {
-
-	@ExceptionHandler(DuplicateException.class)
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public AdviceErrorRespones handleDuplicateKeyException(DuplicateException e) {
-		return setAdviceErrorRespones("duplicate.key", e.getKey() + " duplicate key");
-	}
-
-	@ExceptionHandler(DataNotFoundException.class)
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public AdviceErrorRespones handleAccountNotFoundException(DataNotFoundException e) {
-		return setAdviceErrorRespones("account.not.found", e.getId() + " not found");
-	}
-
-	@ExceptionHandler(BadRequestException.class)
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public AdviceErrorRespones handleBadRequestException(BadRequestException e) {
-		return setAdviceErrorRespones("invalid.parameters", e.getValue() + " :" + e.getMessage());
-	}
-
-	@ExceptionHandler(Exception.class)
-	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-	public AdviceErrorRespones handleException(Exception e) {
-		return setAdviceErrorRespones("unknown.excpetion",  e.getMessage());
-	}
-
-	@ExceptionHandler(NullPointerException.class)
-	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-	public AdviceErrorRespones handleNullPointerException(NullPointerException e) {
-		return setAdviceErrorRespones("null.pointer", e.getMessage());
-	}
-
-	private AdviceErrorRespones setAdviceErrorRespones(String error, String error_description) {
-		return new AdviceErrorRespones(error, error_description);
-	}
-}
+//package me.wonwoo.advice;
+//
+//import org.springframework.http.HttpStatus;
+//import org.springframework.security.access.AccessDeniedException;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.bind.annotation.ResponseStatus;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import me.wonwoo.exception.DataNotFoundException;
+//import me.wonwoo.exception.BadRequestException;
+//import me.wonwoo.exception.DuplicateException;
+//import me.wonwoo.exception.bean.AdviceErrorRespones;
+//
+//@ControllerAdvice
+//@RestController
+//public class GlobalException {
+//
+//	@ExceptionHandler(DuplicateException.class)
+//	@ResponseStatus(HttpStatus.BAD_REQUEST)
+//	public AdviceErrorRespones handleDuplicateKeyException(DuplicateException e) {
+//		return setAdviceErrorRespones("duplicate.key", e.getKey() + " duplicate key");
+//	}
+//
+//	@ExceptionHandler(DataNotFoundException.class)
+//	@ResponseStatus(HttpStatus.BAD_REQUEST)
+//	public AdviceErrorRespones handleAccountNotFoundException(DataNotFoundException e) {
+//		return setAdviceErrorRespones("account.not.found", e.getId() + " not found");
+//	}
+//
+//	@ExceptionHandler(BadRequestException.class)
+//	@ResponseStatus(HttpStatus.BAD_REQUEST)
+//	public AdviceErrorRespones handleBadRequestException(BadRequestException e) {
+//		return setAdviceErrorRespones("invalid.parameters", e.getValue() + " :" + e.getMessage());
+//	}
+//
+//	@ExceptionHandler(Exception.class)
+//	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+//	public AdviceErrorRespones handleException(Exception e) {
+//		return setAdviceErrorRespones("unknown.excpetion",  e.getMessage());
+//	}
+//
+//	@ExceptionHandler(NullPointerException.class)
+//	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+//	public AdviceErrorRespones handleNullPointerException(NullPointerException e) {
+//		return setAdviceErrorRespones("null.pointer", e.getMessage());
+//	}
+//
+//	private AdviceErrorRespones setAdviceErrorRespones(String error, String error_description) {
+//		return new AdviceErrorRespones(error, error_description);
+//	}
+//}
--- src/main/java/me/wonwoo/config/ConfigMvc.java
+++ src/main/java/me/wonwoo/config/ConfigMvc.java
@@ -1,14 +1,19 @@
 package me.wonwoo.config;
 
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.autoconfigure.web.BasicErrorController;
+import org.springframework.boot.autoconfigure.web.DefaultErrorAttributes;
+import org.springframework.boot.autoconfigure.web.ErrorAttributes;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.context.request.RequestAttributes;
 
 @Configuration
 public class ConfigMvc {
@@ -16,20 +21,20 @@ public class ConfigMvc {
 //	 @Autowired
 //	 private AccountsService accountsService;
 
-	/**
-	 * 톰캣 포트
-	 * 
-	 * @return
-	 */
-	@Bean
-	public EmbeddedServletContainerFactory servletContainer() {
-		TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
-		// factory.setPort(8090);
-		factory.setSessionTimeout(10, TimeUnit.MINUTES);
-		// factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND,
-		// "/notfound.html"));
-		return factory;
-	}
+    /**
+     * 톰캣 포트
+     *
+     * @return
+     */
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+        // factory.setPort(8090);
+        factory.setSessionTimeout(10, TimeUnit.MINUTES);
+        // factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND,
+        // "/notfound.html"));
+        return factory;
+    }
 
 //	@Bean
 //	public InitializingBean init() {
@@ -38,24 +43,41 @@ public class ConfigMvc {
 //		};
 //	}
 
-	/**
-	 * 패스워드 인코딩
-	 * 
-	 * @return
-	 */
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
 
-	/**
-	 * 모델맵퍼
-	 * 
-	 * @return
-	 */
-	@Bean
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
-	}
+    @Bean
+    public ErrorAttributes errorAttributes() {
+        return new DefaultErrorAttributes() {
+            @Override
+            public Map<String, Object> getErrorAttributes(RequestAttributes requestAttributes, boolean includeStackTrace) {
+                Map<String, Object> errorAttributes = super.getErrorAttributes(requestAttributes, includeStackTrace);
+                Throwable error = getError(requestAttributes);
+
+//                BasicErrorController
+                // Customize the default entries in errorAttributes to suit your needs
+                return errorAttributes;
+            }
+
+        };
+    }
+
+    /**
+     * 패스워드 인코딩
+     *
+     * @return
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * 모델맵퍼
+     *
+     * @return
+     */
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 
 }
